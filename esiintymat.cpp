f0640c854376a63704c0a4e3f707b780af65a6da
villemie <63785132+villemie@users.noreply.github.com>
2020-04-25 11:00:55 +0300

    Different approact
    Original is not efficeint enough.

--- esiintymat.cpp
+++ esiintymat.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <unordered_map>
 using namespace std;
-int maxN(unordered_map<int, int> &m, unordered_map<int, int>::iterator it )
-{
-    int maxN = 0;
-    int i = 0;
-    for(auto x: m){
-        if(x.second >= maxN){
-            if((x.second != maxN)||(x.first < i)){
-                maxN = x.second;
-                i = x.first;
+void addToMap(set<int> &s,unordered_map<int, int> &m, int add){
+     unordered_map<int, int>::iterator it;
+     if(s.find(add) == s.end()){
+            s.emplace(add);
+        }
+        else{
+            it = m.find(add);
+            if(it != m.end()){
+                it->second++;
+            }
+            else{
+                m.emplace(add,2);
             }
         }
-    }
-    return i;
-
 }
-int main()
-{
-    unordered_map<int, int> m;
+void removeFromMap(set<int> &s, unordered_map<int, int> &m, int remove){
     unordered_map<int, int>::iterator it;
-    vector<int> inputL = {2, 3, 2, 5, 1, 6, 5, 6};
-    vector<int> list;
-    int n = 8;
-    int k = 5;
+    it = m.find(remove);
+    if(it != m.end()){
+        if(it->second < 3){
+            m.erase(it->first);
+        }
+        else{
+            it->second--;
+        }
+    }
+    else{
+        s.erase(remove);
+    }
+}
+//Prepares maps
+void startPrep(set<int> &s, unordered_map<int, int> &m, vector<int> &list, int n, int k){
+    vector<int> inputL = {3,6,4,3,4,3,5,1,1,7};
     int input;
     for (int i = 0; i < n; i++)
     {
@@ -33,40 +44,53 @@ int main()
     }
     for (int i = 0; i < k; i++)
     {
-        it = m.find(list[i]);
-        if (it == m.end())
-        {
-            m.emplace(list[i], 1);
-        }
-        else
-        {
-            it->second++;
+       addToMap(s,m,list[i]);
+    }
+}
+//Gets most common integer
+void maxN(set<int> s, unordered_map<int, int> m){
+unordered_map<int, int>::iterator it;
+//if m is empty there is no duplicates, lowest int will be chosen
+if(m.empty() == true){
+    for(auto x: s){
+        cout << x << " ";
+        break;
+    }
+}
+else{
+    int maxI;
+    int maxCount = 0;
+    for(auto x: m){
+        
+        if((x.second > maxCount)||((x.second == maxCount)&&(x.first < maxI))){
+            maxCount = x.second;
+            maxI = x.first;
         }
     }
-    int h = maxN(m,it);
-    cout << h << " ";
+    cout << maxI << " ";
+
+}
+}
+
+int main()
+{
+    //All ints are stored in set s
+    set<int> s;
+    //If amount of ceraint int is over 1 it is also stored in map m
+    unordered_map<int, int> m;
+    unordered_map<int, int>::iterator it;
+    vector<int> list;
+    int n = 10;
+    //cin >> n;
+    int k = 5;
+    //cin >> k;
+    startPrep(s,m,list,n,k);
+    maxN(s,m);
     int i = 0;
-    while(k<n){
-        //Poistetaan i kuvaajasta
-        //Jos lisattava ja poistettava ovat samat, siirrtyaan suoraan tulostukseen
-        if(list[i]!=list[k]){
-            it = m.find(list[i]);
-            if(it->second < 2){
-                m.erase(it->first);
-            }
-            else{
-                it->second--;
-            }
-            it=m.find(list[k]);
-            if(it==m.end()){
-                m.emplace(list[k],1);
-            }
-            else{
-                it->second++;
-            }
-            h = maxN(m,it);
-        }
-        cout << h << " ";
+    while(k < n){
+            removeFromMap(s,m,list[i]);
+            addToMap(s,m,list[k]);
+            maxN(s,m);
         i++;
         k++;
     }
